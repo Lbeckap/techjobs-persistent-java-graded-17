6124de70c07acdef7d757d07b93a04a1670a6828
Lori <148814155+Lbeckap@users.noreply.github.com>
2024-11-04 23:11:06 -0500

    Task 2 completed

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,26 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @Size(max = 255, message = "Description must be less than 255 characters.")
+    private String description;
+
+    public Skill() {
+    }
+
+    public Skill(String description) {
+        this.description = description;
+    }
+
+    public @Size(max = 255, message = "Description must be less than 255 characters.") String getDescription() {
+        return description;
+    }
+
+    public void setDescription(@Size(max = 255, message = "Description must be less than 255 characters.") String description) {
+        this.description = description;
+    }
 }
